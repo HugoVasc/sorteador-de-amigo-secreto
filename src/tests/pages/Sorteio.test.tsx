c398e8117dccfc71664a0c7bca6d83221786a0bc
Hugo Vasconcelos <hugo.silva08@hotmail.com>
2022-08-21 15:54:14 -0300

    feat - creating tests and the functionality to disappear with the selected name after 5 seconds

--- src/tests/pages/Sorteio.test.tsx
+++ src/tests/pages/Sorteio.test.tsx
@@ -1,5 +1,6 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
+import { act } from "react-dom/test-utils";
 import { RecoilRoot } from "recoil";
 import Sorteio from "../../pages/Sorteio/Sorteio";
 import { useListaDeParticipantes } from "../../state/hooks/useListaDeParticipantes";
@@ -51,5 +52,28 @@ describe("Sorteio page", () => {
     fireEvent.click(button);
     const secretFriend = screen.getByRole("alert");
     expect(secretFriend).toBeInTheDocument();
+  });
+  test("Should hide the selected name after five seconds", () => {
+    jest.useFakeTimers();
+    render(
+      <RecoilRoot>
+        <Sorteio />
+      </RecoilRoot>
+    );
+    const select = screen.getByPlaceholderText("Selecione o seu nome");
+    fireEvent.change(select, {
+      target: {
+        value: participantes[0],
+      },
+    });
+    const button = screen.getByRole("button");
+    fireEvent.click(button);
+    let secretFriend = screen.queryByRole("alert");
+    expect(secretFriend).toBeInTheDocument();
+    act(() => {
+      jest.runAllTimers();
+    });
+    secretFriend = screen.queryByRole("alert");
+    expect(secretFriend).toBeNull();
   });
 });
